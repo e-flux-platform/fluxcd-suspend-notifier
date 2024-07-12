e1e156e2fe8d5dd911686c130739044253a8d723
Nicholas Jones <me@nicholasjon.es>
2024-07-12 18:03:53 +0100

    Add path to log line

--- internal/k8s/client.go
+++ internal/k8s/client.go
@@ -43,7 +43,7 @@ func NewClient(configPath string) (*Client, error) {
 func (c *Client) GetRawResource(ctx context.Context, relativePath string) (map[string]any, error) {
 	body, err := c.client.RESTClient().Get().AbsPath(path.Join("apis", relativePath)).DoRaw(ctx)
 	if err != nil {
-		slog.Warn("failed to fetch resource", slog.Any("error", err))
+		slog.Warn("failed to fetch resource", slog.Any("error", err), slog.Any("path", path.Join("apis", relativePath)))
 		return nil, err
 	}
 
